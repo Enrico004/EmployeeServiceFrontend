0249a5730bfe683bbdf28cd13ce239d6af8b76ab
Enrico Wiegmann <enrico.wiegmann@ssi-schaefer.com>
2024-01-11 13:23:23 +0100

    WIP Keycloak

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,30 @@
-import {ApplicationConfig, importProvidersFrom} from '@angular/core';
+import { APP_INITIALIZER,ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { KeycloakAngularModule,KeycloakService } from 'keycloak-angular';
+
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes),importProvidersFrom()]
+  providers: [provideRouter(routes),
+    KeycloakService,{
+    provide:APP_INITIALIZER,
+    useFactory: initializeKeycloak,
+    multi:true,
+    deps: [KeycloakService] 
+  }]
 };
+
+function initializeKeycloak(keycloak:KeycloakService){
+  return () =>
+    keycloak.init({
+      config:{
+        url:" https://keycloak.szut.dev/auth",
+        realm:"szut",
+        clientId:"employee-management-service-frontend"
+      },
+      initOptions:{
+        onLoad:"login-required",
+      }
+    })
+}
