9a23909597389385bec9d78edacb6b6e556fea58
Milan.Kiffmeyer <milan36@schule.bremen.de>
2024-01-25 10:21:36 +0100

    Enricos fehler ausbügeln

--- src/app/model/EmployeeWithSkillID.ts
+++ src/app/model/EmployeeWithSkillID.ts
@@ -1,4 +1,6 @@
-export class EmployeeWithSkillID {
+/**
+ class EmployeeWithSkillID {
   //public id: number,
   constructor(public lastName?: string, public firstName?: string, public street?: string, public city?: string,public postcode?: string, public phone?: string, public skillSet?: number[]) {}
 }
+**/
